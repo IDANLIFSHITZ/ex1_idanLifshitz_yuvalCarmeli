52923ecda0086c668816482dbaf698a5e50b44d5
‪Yoad Abels‬‏ <7530950-yoadhamagniv@users.noreply.gitlab.com>
2023-05-04 10:47:03 +0300

    added stuff

--- HackEnrollment.c
+++ HackEnrollment.c
@@ -4,11 +4,17 @@
 #include "IsraeliQueue.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 
 
 
 #define ID_SIZE 9
+
+#define FRIEND 20
+#define  ENEMY -20
+#define NEITHER 0
+
+#define NINTH_DIGIT 1000000
 
 typedef struct Student{
     char* StudentID;
@@ -42,6 +48,13 @@ typedef struct EnrollmentSystem {
 
 student createNewStudent();
 void initStudentArrayOfEnrollmentSystem(EnrollmentSystem sys, FILE* students);
+course createNewCourse(char* courseNumber);
+int compFunc(student s1, student s2);
+int friendshipFuncHackerFile(student s1, student s2);
+int friendshipFuncNameDist(student s1, student s2);
+int friendshipFuncIDSubtract(student s1, student s2);
+int calcNameDiff(char* name1, char* name2);
+int friendshipFuncHackerFileHelper(student s1, student s2);
 
 
 EnrollmentSystem createEnrollment(FILE* students, FILE* courses, FILE* hackers){
@@ -191,11 +204,13 @@ student createNewStudent(){
 }
 
 
-void initCoursesArrayOfSystem(EnrollmentSystem sys, FILE* courses) {
+void initCoursesArrayOfSystem(EnrollmentSystem sys, FILE* courses)
+{
 
 }
 
-void initHackersArrayOfSystem(EnrollmentSystem sys, FILE* hackers){
+void initHackersArrayOfSystem(EnrollmentSystem sys, FILE* hackers)
+{
 
 }
 
@@ -207,13 +222,14 @@ EnrollmentSystem readEnrollment(EnrollmentSystem sys, FILE* queues)
     while (!isEOF)
     {
         char* currName = (char*) malloc(ID_SIZE);
+        int currCourseNum = 0;
         bool isCourseName = true;
         while (fscanf(queues, "%[^\n] ", currName))
         {
             if (isCourseName)
             {
                 isCourseName = false;
-                sys.
+                sys->course.createCourse()
             }
             else
 
@@ -229,4 +245,109 @@ void hackEnrollment(EnrollmentSystem sys, FILE* out)
 
 }
 
+int friendshipFuncHackerFile(student s1, student s2)
+{
+    int res = 0;
+    if (s1->friendsId != NULL)
+    {
+        res = friendshipFuncHackerFile(s1, s2);       
+    }
+    else if (s2->friendsId != NULL)
+    {
+        res = friendshipFuncHackerFile(s2, s1);
+    }
+    return res;
+}
+
+int friendshipFuncHackerFileHelper(student s1, student s2)
+{
+    for (int i = 0; s1->friendsId[i] != NULL; i++)
+    {
+        if (compFunc(s2, s1->friendsId[i])) // if s2 is friend.
+        {
+            return FRIEND;
+        }
+    }
+    for (int i = 0; s1->enemyId[i] != NULL; i++)
+    {
+        if (compFunc(s2, s1->friendsId[i])) // if s2 is enemy.
+        {
+            return ENEMY;
+        }
+    }
+    return NEITHER;
+}
+
+
+int friendshipFuncNameDist(student s1, student s2)
+{
+    int sum = 0;
+    if (strlen(s1->name) >= strlen(s2->name)) // if first student name is longer.
+    {
+        sum += calcNameDiff(s1->name, s2->name);
+    }
+    else
+    {
+        sum += calcNameDiff(s2->name, s1->name);
+    }
+    if (strlen(s1->surName) >= strlen(s2->surName)) // if first student surname is longer.
+    {
+        sum += calcNameDiff(s1->surName, s2->surName);
+    }
+    else
+    {
+        sum += calcNameDiff(s2->surName, s1->surName);
+    }
+    return sum;
+}
+
+int calcNameDiff(char* name1, char* name2)
+{
+    int sum = 0;
+    for (int i = 0; i < strlen(name1); i++)
+    {
+        int curr = name1[i];
+        if (i < strlen(name2))
+        {
+            curr += name2[i];
+        }
+        sum += abs(curr);
+    }
+    return sum;
+}
+
+int friendshipFuncIDSubtract(student s1, student s2)
+{
+    int id1 = 0, id2 = 0;
+    for (int i = 1; i < 9; i++) //for digits 1-8 in ID of s1 and s2.
+    {
+        id1 *= 10;
+        id2 *= 10;
+        id1 += s1->StudentID[i];
+        id2 += s2->StudentID[i];
+    }
+    if (s1->StudentID[0] == '-') // if negative ID.
+    {
+        id1 *= -1;
+    }
+    else
+    {
+        id1 += NINTH_DIGIT * s1->StudentID[0];
+    }
+    if (s2->StudentID[0] == '-') // if negative ID.
+    {
+        id2 *= -1;
+    }
+    else
+    {
+        id2 += NINTH_DIGIT * s1->StudentID[0];
+    }
+
+    return abs(id1-id2); // returns abs of difference between IDs.
+}
+
+int compFunc(student s1, student s2)
+{
+    return (!strcmp(s1->StudentID, s2->StudentID)); // compare between student IDs.
+}
 
